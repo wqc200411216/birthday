cd7cd92a1882c2bc3a015f14e450622b1f7ec7dd
wqc200411216 <165015488+wqc200411216@users.noreply.github.com>
2025-10-30 18:27:26 +0800

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ var config = {
      */
     imgs: {
         "心爱的小可爱": "./imgs/head.png",
-        // "今天是你的生日": "./imgs/birthday.jpg",
+        "今天是你的生日": "./imgs/head02.png",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
